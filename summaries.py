9383806a092fb58bb9f3f471e078c75ac8f07a5b
catalin.mates <catalin.mates@saucelabs.com>
2023-04-19 11:16:41 -0400

    full summary code

--- summaries.py
+++ summaries.py
@@ -71,89 +71,89 @@ chart = (
 st.altair_chart(chart, use_container_width=True)
 
 
-# # Show summary between actual and exected
-# st.subheader("Actual ARR vs Expected ARR by ccy_bins: ")
-# st.write(actual_vs_expected)
+# Show summary between actual and exected
+st.subheader("Actual ARR vs Expected ARR by ccy_bins: ")
+st.write(actual_vs_expected)
 
-# # Takeaways
-# st.subheader("Takeaways: ")
+# Takeaways
+st.subheader("Takeaways: ")
 
-# st.write(
-#     """
-# - The summary of actual ARR versus expected ARR highlights the percentage differences between the actual ARR and the expected ARR. What could be the reason for the significantly lower average prices in the 1-9 and 25-49 bins compared to the actual ARR?
-#     - 1-9 ccy_bin shows 20% less actual median ARR vs expected arr.
-#     - 25-49 ccy_bin shows 18% less actual median ARR vs expected arr.
-#     """
-# )
+st.write(
+    """
+- The summary of actual ARR versus expected ARR highlights the percentage differences between the actual ARR and the expected ARR. What could be the reason for the significantly lower average prices in the 1-9 and 25-49 bins compared to the actual ARR?
+    - 1-9 ccy_bin shows 20% less actual median ARR vs expected arr.
+    - 25-49 ccy_bin shows 18% less actual median ARR vs expected arr.
+    """
+)
 
-# st.write(
-#     """
-# - The box plot reveals the presence of numerous outliers. Should these data points be interpreted as anomalies or as legitimate deviations from the median?
-#     - Certain data points appear to be errors, as they indicate that the total ARR for VM exceeds the combined total ARR for both VM and RDC. Logically, this discrepancy should not occur.
-#     """
-# )
+st.write(
+    """
+- The box plot reveals the presence of numerous outliers. Should these data points be interpreted as anomalies or as legitimate deviations from the median?
+    - Certain data points appear to be errors, as they indicate that the total ARR for VM exceeds the combined total ARR for both VM and RDC. Logically, this discrepancy should not occur.
+    """
+)
 
-# # Filter our the clients with VM ARR > Total arr
-# arr_ind_false = arr[arr["arr_ind"] == False].sort_values(
-#     by=["arr_per_vm"], ascending=False
-# )
+# Filter our the clients with VM ARR > Total arr
+arr_ind_false = arr[arr["arr_ind"] == False].sort_values(
+    by=["arr_per_vm"], ascending=False
+)
 
-# arr_ind_true = arr[arr["arr_ind"] == True].sort_values(
-#     by=["arr_per_vm"], ascending=False
-# )
+arr_ind_true = arr[arr["arr_ind"] == True].sort_values(
+    by=["arr_per_vm"], ascending=False
+)
 
 
-# # Comments for the filtered data
-# st.subheader("Clients with VM ARR > Total ARR:")
-# st.write(arr_ind_false)
+# Comments for the filtered data
+st.subheader("Clients with VM ARR > Total ARR:")
+st.write(arr_ind_false)
 
-# st.write(
-#     """
-# - Based on total VM ARR, the highest arr_per_vm is over $27000.
-#     - 13 clients show arr_per_vm over $4000.
-#     """
-# )
+st.write(
+    """
+- Based on total VM ARR, the highest arr_per_vm is over $27000.
+    - 13 clients show arr_per_vm over $4000.
+    """
+)
 
-# st.write(
-#     """
-# - Once we remove these points the box plot looks like this:
-#     """
-# )
+st.write(
+    """
+- Once we remove these points the box plot looks like this:
+    """
+)
 
-# st.subheader("ARR per VM by ccy_bins when VM ARR < Total ARR: ")
-# chart_ind_true = (
-#     alt.Chart(arr_ind_true)
-#     .mark_boxplot()
-#     .encode(
-#         x=alt.X("ccy_bins:N", sort=alt.EncodingSortField("vm_ccy_total")),
-#         y="arr_per_vm:Q",
-#     )
-# )
+st.subheader("ARR per VM by ccy_bins when VM ARR < Total ARR: ")
+chart_ind_true = (
+    alt.Chart(arr_ind_true)
+    .mark_boxplot()
+    .encode(
+        x=alt.X("ccy_bins:N", sort=alt.EncodingSortField("vm_ccy_total")),
+        y="arr_per_vm:Q",
+    )
+)
 
-# # display the chart in Streamlit
-# st.altair_chart(chart_ind_true, use_container_width=True, theme="streamlit")
+# display the chart in Streamlit
+st.altair_chart(chart_ind_true, use_container_width=True, theme="streamlit")
 
-# # Summarize arr by ccy_bins when arr_ind = True
-# actual_vs_expected_true = (
-#     arr_ind_true.groupby(["ccy_bins"])["arr_per_vm"]
-#     .agg(["count", "median", "mean", "min", "max"])
-#     .reset_index()
-# )
-# actual_vs_expected_true["vm_price_annual"] = [
-#     200 * 12,
-#     174 * 12,
-#     159 * 12,
-#     139 * 12,
-#     119 * 12,
-#     99 * 12,
-#     85 * 12,
-# ]
-# actual_vs_expected_true["median_vs_vm_price"] = (
-#     actual_vs_expected_true["median"] / actual_vs_expected_true["vm_price_annual"]
-# )
-# actual_vs_expected_true["mean_vs_vm_price"] = (
-#     actual_vs_expected_true["mean"] / actual_vs_expected_true["vm_price_annual"]
-# )
+# Summarize arr by ccy_bins when arr_ind = True
+actual_vs_expected_true = (
+    arr_ind_true.groupby(["ccy_bins"])["arr_per_vm"]
+    .agg(["count", "median", "mean", "min", "max"])
+    .reset_index()
+)
+actual_vs_expected_true["vm_price_annual"] = [
+    200 * 12,
+    174 * 12,
+    159 * 12,
+    139 * 12,
+    119 * 12,
+    99 * 12,
+    85 * 12,
+]
+actual_vs_expected_true["median_vs_vm_price"] = (
+    actual_vs_expected_true["median"] / actual_vs_expected_true["vm_price_annual"]
+)
+actual_vs_expected_true["mean_vs_vm_price"] = (
+    actual_vs_expected_true["mean"] / actual_vs_expected_true["vm_price_annual"]
+)
 
-# st.subheader("Actual ARR vs Expected ARR by ccy_bins when VM ARR < Total ARR: ")
-# st.write(actual_vs_expected_true)
+st.subheader("Actual ARR vs Expected ARR by ccy_bins when VM ARR < Total ARR: ")
+st.write(actual_vs_expected_true)
